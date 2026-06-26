3aab583e22544b2dbbe49bdb0e051156addfd9cd
mecom8489 <mecom8489@gmail.com>
2026-06-26 15:14:55 +0530

    feat: implement axios instance with JWT authentication interceptor and GET request deduplication

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: `http://${window.location.hostname}:5001/api`,
+  baseURL: `https://api.sivaathri.co.in/api`,
   headers: {
     'Content-Type': 'application/json',
   },
